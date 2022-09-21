73f6fec39d903d2843c524308b70637f8cf940d6
Zoltán Bedi <zoltan.bedi@gmail.com>
2022-09-21 21:32:47 +0200

    Test

--- netlify/functions/fb-post.ts
+++ netlify/functions/fb-post.ts
@@ -1,25 +1,26 @@
 import { Handler } from '@netlify/functions'
-import { Octokit } from 'octokit'
+// import { Octokit } from 'octokit'
 
 const handler: Handler = async (event) => {
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
-  })
+  //   const octokit = new Octokit({
+  //     auth: process.env.GITHUB_TOKEN,
+  //   })
 
-  const response = await octokit.request('PUT /repos/zoltanbedi/pianobar.hu/contents/src/data', {
-    owner: 'zoltanbedi',
-    repo: 'pianobar.hu',
-    path: 'src/data',
-    message: 'Add new post',
-    committer: {
-      name: 'Zoltan Bedi',
-      email: 'zoltan.bedi@gmail.com',
-    },
-    content: event.isBase64Encoded ? event.body : Buffer.from(event.body || '').toString('base64'),
-  })
+  //   const response = await octokit.request('PUT /repos/zoltanbedi/pianobar.hu/contents/src/data', {
+  //     owner: 'zoltanbedi',
+  //     repo: 'pianobar.hu',
+  //     path: 'src/data',
+  //     message: 'Add new post',
+  //     committer: {
+  //       name: 'Zoltan Bedi',
+  //       email: 'zoltan.bedi@gmail.com',
+  //     },
+  //     content: event.isBase64Encoded ? event.body : Buffer.from(event.body || '').toString('base64'),
+  //   })
+  console.log(event.body)
   return {
-    statusCode: response.status,
-    body: response.data,
+    statusCode: 200,
+    body: 'It worked',
   }
 }
 
